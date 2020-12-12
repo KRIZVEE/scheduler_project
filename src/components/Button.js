353dd4d8f4298b5aca973c44d40ff5030b02c456
KRIZVEE <rizveekashif4u@gmail.com>
2020-12-12 02:59:22 -0500

    adding building a button activity

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,28 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+
+   if(props.confirm) {
+      buttonClass += " button--confirm";
+   }
+
+   if(props.danger) {
+      buttonClass += " button--danger";
+   }
+
+   if(props.disable) {
+      buttonClass += "button-disable";
+   }
+
+   return (
+   <button 
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+   >
+   
+      {props.children}
+   </button>
+   );
 }
